0b152fd319d56f6850dd75244b8e6cd52e2ed39f
Juan Miguel Vega Caro <jm.vegac@uniandes.edu.co>
2022-02-19 01:53:50 -0500

    Modified Thread class

--- package1/T.java
+++ package1/T.java
@@ -1,17 +1,62 @@
+/**
+ * Caso 1 Infraestructura Computacional "Mailbox"
+ * 
+ * @author Juan Miguel Vega Caro 201715344
+ * @author  
+ */
+import java.util.ArrayList;
+import java.util.LinkedList; 
+
+/**
+ * Clase Thread
+ */
 public class T extends Thread{
 
-    private int id; 
 
-
-    private Buzon buzon; 
-
-    public void consumir(){
-
-    }
-
-
-    
-    }
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////                                                 ATRIBUTES                                                             /////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
  
+
+    //Int value which represents the delay in millis of the thread 
+    private int delay; 
+
+    //Buzon object that represents the left mailbox of the thread
+    private Buzon left;
+    
+    //Buzon object that represents the left mailbox of the thread
+    private Buzon right;
+
+    //Boolean var that represents the type of receive operation 
+    private boolean receive;
+
+    //Boolean var thar represents the type of send operation
+    private boolean send; 
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////                                                 CONSTRUCTOR                                                           /////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /**
+     * 
+     * @param pDelay
+     * @param pLeft
+     * @param pRight
+     */
+    public T(int pDelay, Buzon pLeft, Buzon pRight){
+        this.delay = pDelay;
+
+        this.left = pLeft;
+
+        this.right = pRight;
+    }
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////                                                      RUN                                                              /////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public void run(){
+        
+    }
 
 }
