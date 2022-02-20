dae6a0dc6d3d71aada1a8d12ca46348bf942d0ba
Juan Miguel Vega Caro <jm.vegac@uniandes.edu.co>
2022-02-19 23:17:42 -0500

    Edited Buffer class

--- package1/Buzon.java
+++ package1/Buzon.java
@@ -10,25 +10,55 @@ import java.util.LinkedList;
 /**
  * Clase Buzon, en este caso la clase buzon actua como buffer, es el encargado de "Producir" y "Consumir" los mensajes 
  */
+
 public class Buzon {
-    //Almacena los mensajes del buzon
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////                                                 ATRIBUTES                                                             /////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    //List that stores the buffer messages
     LinkedList<Mensaje> list = new LinkedList<Mensaje>(); 
 
-    //Capacidad maxima de cada buzon 
+    //Capacity of each buffer 
     private int capacity; 
 
-    //
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////                                                 CONSTRUCTOR                                                           /////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Construct the buffer 
+     * @param pCapacity
+     */
     public Buzon(int pCapacity){
          this.capacity = pCapacity; 
     }
 
-    public synchronized void producir(){
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////                                                    METHODS                                                            /////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    
+    public synchronized void producirActivo(Mensaje pMensaje){
+        
+    }
+
+    public synchronized void producirPasivo(Mensaje pMensaje){
 
     }
 
-    public synchronized void consumir(){
+
+    public synchronized void consumirActivo(Mensaje pMensaje){
 
     }
+
+
+    public synchronized void consumirPasivo(Mensaje pMensaje){
+
+    }
+
 
     public boolean hayMensajes(){
         return list.size() > 0 ;
--- package1/Mensaje.java
+++ package1/Mensaje.java
@@ -1,3 +1,15 @@
+/**
+ * Caso 1 Infraestructura Computacional "Mailbox"
+ * 
+ * @author Juan Miguel Vega Caro 201715344
+ * @author  
+ */
+import java.util.ArrayList;
+import java.util.LinkedList; 
+
+/**
+ * Clase mensaje, contiene las firmas de los buzones.
+ */
 public class Mensaje {
     private String mensaje; 
 
